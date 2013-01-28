ccd7dd6476d4087010e2daa5b0728495a2e17d84
evan <evanrthom@gmail.com>
2013-01-27 23:34:14 -0500

    caching on applicationCacheTest works

---
+++ applicationCacheTest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicationCacheTest::Application.config.secret_token = 'b1741485f31805d83178584ca0b46ddd81b515d8975d7a8d1d15749fcf1c15df870cb9f2a9d8458834f1cfc5b7d9e604ae2f4e6037ed731e5897dff659de2fa1'
